1c53c0a61716e12211edb8cdfb21f59271e03381
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-05-21 16:10:31 +0600

    MongoDB database functionality added

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL= 
+    'mongodb+srv://root:3924@book-store-mern.wmnuwew.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-Mern';
