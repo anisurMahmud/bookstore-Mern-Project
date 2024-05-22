c38fd7919feb5f85d80ae2cd8c8e0df5bb62766e
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-05-22 14:33:36 +0600

    cors, vite, tailwind css added

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,27 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
-import booksRoute from './routes/booksRoute.js'
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
+
+//middleware for parsing body as json
 app.use(express.json());
+
+//middleware for handling cors
+//first way: allow all origin with default cors 
+// app.use(cors());
+
+//second way: allow custom origins
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        method: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
 app.get( '/', (request, response) =>{
     console.log(request);
     return response.status(234).send('Welcome to Mern Stack Tutorial');
